d5ee8b599bb8bc39ed0181db06d60cb44a880d23
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-24 23:00:02 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,8 +1,3 @@
-def say_hello
-
-    puts "Hello, #{name}"
-  end
-
-  def greeting(name="Ruby programmer", language="Ruby")
-    puts "Hello, #{name}. We heard you are a great #{language} programmer."
-  end
+def greeting(name="Ruby programmer", language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
