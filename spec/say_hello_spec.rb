4389c1e6ad375b2dd2a79110c8bfef82cb8bbeb6
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-24 22:56:06 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,10 +1,8 @@
 require_relative './spec_helper'
 
-describe "say_hello" do 
+def "say_hello" do 
   
-  it 'accepts an argument of a name and prints out Hello with that Name' do
-    expect($stdout).to receive(:puts).with("Hello Kent Beck!")
-    say_hello("Kent Beck")
+    puts "Hello, #{name}"
   end
 
   it 'defaults to Ruby Programmer when no name is passed in' do
@@ -12,3 +10,5 @@ describe "say_hello" do
     say_hello()
   end
 end
+
+
